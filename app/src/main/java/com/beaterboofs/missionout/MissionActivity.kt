def3d2817e76b335bcc707e5fa9d378315e7af1a
jeromoney <justin.matis.com@gmail.com>
2019-10-27 15:42:27 -0600

    extracted create mission file

--- app/src/main/java/com/beaterboofs/missionout/MissionActivity.kt
+++ app/src/main/java/com/beaterboofs/missionout/MissionActivity.kt
@@ -2,6 +2,7 @@ package com.beaterboofs.missionout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.beaterboofs.missionout.ui.mission.DisplayMissionFragment
 
 class MissionActivity : AppCompatActivity() {
@@ -9,9 +10,17 @@ class MissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mission_activity)
+        val create_mission = intent.getBooleanExtra("create_mission", false)
+        var fragment : Fragment
+        if (create_mission){
+            fragment = CreateMissionFragment.newInstance()
+        }
+        else {
+            fragment = DisplayMissionFragment.newInstance()
+        }
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, DisplayMissionFragment.newInstance())
+                .replace(R.id.container, fragment)
                 .commitNow()
         }
     }
--- app/src/main/java/com/beaterboofs/missionout/ui/mission/DisplayMissionFragment.kt
+++ app/src/main/java/com/beaterboofs/missionout/ui/mission/DisplayMissionFragment.kt
@@ -13,15 +13,12 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.beaterboofs.FirestoreRepository
 import com.beaterboofs.missionout.BR
-import com.beaterboofs.missionout.Mission
 import com.beaterboofs.missionout.MissionActivity
 import com.beaterboofs.missionout.R
 import com.beaterboofs.missionout.databinding.FragmentMissionDisplayBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.textview.MaterialTextView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.iid.FirebaseInstanceId
 
 import kotlinx.android.synthetic.main.fragment_mission_display.*
@@ -66,41 +63,6 @@ class DisplayMissionFragment : Fragment() {
         val binding: FragmentMissionDisplayBinding =
             DataBindingUtil.setContentView<FragmentMissionDisplayBinding>(this.requireActivity(), R.layout.fragment_mission_display)
         binding.lifecycleOwner = this
-        // set onClickListener for FAB
-        binding.createMission.setOnClickListener{
-            // submit mission to firestore database
-            val isDataValid = true
-            if (isDataValid){
-                // create object from input text TODO - use todo to simplify code
-                val description = binding.missionDescriptionEditTextVal.text.toString()
-                val needForAction = binding.needForActionEditTextVal.text.toString()
-                val locationDescription = binding.locationDescriptionEditTextVal.text.toString()
-                val lat = binding.latEditTextVal.text.toString().toDouble()
-                val lon = binding.lonEditTextVal.text.toString().toDouble()
-                val geopoint = GeoPoint(lat, lon)
-
-                val mission = Mission(
-                    description = description,
-                    location = geopoint,
-                    locationDescription = locationDescription,
-                    needForAction = needForAction,
-                    docId = null,
-                    responseMap = null)
-                var db = FirebaseFirestore.getInstance()
-                db.collection("/teams/raux5KIhuIL84bBmPSPs/missions").add(mission) // TODO - customize document field
-                    .addOnSuccessListener {docRef ->
-                    // update worked so hide edit views
-                        binding.editTextGroup.visibility = View.GONE
-                        binding.textViewGroup.visibility = View.VISIBLE
-                        viewModel.docId = docRef.id
-                    }
-                    .addOnFailureListener {
-                        //update did not work
-                    }
-            }
-
-        }
-
 
        // set up dropdown box
         val respondingList = resources.getStringArray(R.array.responding_dropdown_menu)
@@ -118,19 +80,8 @@ class DisplayMissionFragment : Fragment() {
                 FirestoreRepository.sendResponse(response!!, viewModel.docId!!)
             }
         }
+        viewModel.docId = (context as MissionActivity).intent.getStringExtra("mission_id")
 
-
-        val createMission = (context as MissionActivity).intent.getBooleanExtra("create_mission", false)
-        if (createMission){
-            // show the edit text fields and hide the edit text fields
-            binding.editTextGroup.visibility = View.VISIBLE
-            binding.textViewGroup.visibility = View.GONE
-        }
-        else{
-            binding.editTextGroup.visibility = View.GONE
-            binding.textViewGroup.visibility = View.VISIBLE
-            viewModel.docId = (context as MissionActivity).intent.getStringExtra("mission_id")
-        }
         viewModel.getMission().observe(this, Observer {
             // update UI
             binding.setVariable(BR.missionInstance, viewModel.getMission().value)
